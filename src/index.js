5fb83c2783df30566f39a039aab5a257140a87ad
zengjie <thezj@vip.qq.com>
2017-08-18 13:38:05 +0800

    propstype

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 const React = require('react')
 const ReactDom = require('react-dom')
+const PropTypes = require('prop-types')
 
 document.body.innerHTML = '<div id="root"></div>'
 
@@ -11,9 +12,17 @@ const componentsObject = {
     }
 }
 
+componentsObject.DatePicker.propTypes = {
+    color:PropTypes.string
+}
+
+componentsObject.DatePicker.defaultProps = {
+    color:'yellow'
+}
+
 let helloworld = React.createElement('h1', {
     style: {
         border: '1px dotted red'
     }
-}, ['hello world',<componentsObject.DatePicker key='ip1' color='red'/>,React.createElement(componentsObject.DatePicker,{color:'blue',key:'ip2'},null)])
+}, ['hello world',<componentsObject.DatePicker key='ip1'/>,React.createElement(componentsObject.DatePicker,{color:10000,key:'ip2'},null)])
 ReactDom.render(helloworld, document.querySelector('#root'))
