4e3f007c59b17f7beadc7af62eb9de1be2d2a5e5
zengjie <thezj@vip.qq.com>
2017-08-22 14:26:09 +0800

    uncontrolled component

--- src/index.js
+++ src/index.js
@@ -47,9 +47,28 @@ class AutoFocusTextInput extends React.Component{
     }
 }
 
+class NameForm extends React.Component{
+    handleSubmit(e){
+        console.info(`a name was submitted:${this.input.value}`)
+        e.preventDefault()
+    }
+
+    render(){
+        return(
+            <form onSubmit={e=>this.handleSubmit(e)}>
+                <lable>
+                    {/* use defaultValue to specify the initial value (or defaultChecked for checkbox or radio) */}
+                    name:<input defaultValue='Jim' type='text' ref={e=>this.input = e} /> 
+                </lable>
+                <input type='submit' value='Submit U Name' />
+            </form>
+        )
+    }
+}
+
 let helloworld = React.createElement('h1', {
     style: {
         border: '1px dotted red'
     }
-}, [<AutoFocusTextInput key='ip3'></AutoFocusTextInput>,<CustomTextInput key='ip0'></CustomTextInput>,'hello world',<componentsObject.DatePicker key='ip1'/>,React.createElement(componentsObject.DatePicker,{color:10000,key:'ip2'},null)])
+}, [<NameForm key='ip4'></NameForm>,<AutoFocusTextInput key='ip3'></AutoFocusTextInput>,<CustomTextInput key='ip0'></CustomTextInput>,'hello world',<componentsObject.DatePicker key='ip1'/>,React.createElement(componentsObject.DatePicker,{color:10000,key:'ip2'},null)])
 ReactDom.render(helloworld, document.querySelector('#root'))
